1f380697350b719fed7cd255a0e67f727c525065
Krzysztof Maczkowiak <k.maczkowiak@webweit.de>
2020-07-17 12:50:27 +0200

    test

--- src/Tests/bootstrap.php
+++ src/Tests/bootstrap.php
@@ -10,19 +10,19 @@ require \dirname(__DIR__) . '/../vendor/autoload.php';
 
 if (\file_exists(\dirname(__DIR__) . '/../config/bootstrap.php')) {
     require \dirname(__DIR__) . '/../config/bootstrap.php';
-} elseif (\method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(\dirname(__DIR__) . '/../.env');
+} elseif (\method_exists(Dotenv::class, 'loadEnv')) {
+    (new Dotenv())->loadEnv(\dirname(__DIR__) . '/../.env');
 }
 
-if (!isset($_ENV['WEB_FACT_FINDER_API_USERNAME'])) {
-    $_ENV['WEB_FACT_FINDER_API_USERNAME'] = getenv('WEB_FACT_FINDER_API_USERNAME');
-}
-if (!isset($_ENV['WEB_FACT_FINDER_API_PASSWORD'])) {
-    $_ENV['WEB_FACT_FINDER_API_PASSWORD'] = getenv('WEB_FACT_FINDER_API_PASSWORD');
-}
-if (!isset($_ENV['WEB_FACT_FINDER_API_URL'])) {
-    $_ENV['WEB_FACT_FINDER_API_URL'] = getenv('WEB_FACT_FINDER_API_URL');
-}
-if (!isset($_ENV['WEB_FACT_FINDER_API_TEST_CHANNEL'])) {
-    $_ENV['WEB_FACT_FINDER_API_TEST_CHANNEL'] = getenv('WEB_FACT_FINDER_API_TEST_CHANNEL');
-}
+//if (!isset($_ENV['WEB_FACT_FINDER_API_USERNAME'])) {
+//    $_ENV['WEB_FACT_FINDER_API_USERNAME'] = getenv('WEB_FACT_FINDER_API_USERNAME');
+//}
+//if (!isset($_ENV['WEB_FACT_FINDER_API_PASSWORD'])) {
+//    $_ENV['WEB_FACT_FINDER_API_PASSWORD'] = getenv('WEB_FACT_FINDER_API_PASSWORD');
+//}
+//if (!isset($_ENV['WEB_FACT_FINDER_API_URL'])) {
+//    $_ENV['WEB_FACT_FINDER_API_URL'] = getenv('WEB_FACT_FINDER_API_URL');
+//}
+//if (!isset($_ENV['WEB_FACT_FINDER_API_TEST_CHANNEL'])) {
+//    $_ENV['WEB_FACT_FINDER_API_TEST_CHANNEL'] = getenv('WEB_FACT_FINDER_API_TEST_CHANNEL');
+//}
